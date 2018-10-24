f7ad430794015a07b2840615ee2d9456bce23bf6
AsaZws <zhouws.com@gmail.com>
2018-10-24 18:09:19 +0800

    first-commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+
+// 箭头的class
+// var oArrow = document.getElementsByClassName('.arrow');
+
+// oArrow.style.bottom = 0;
+// oArrow.style = window.screen.availWidth-100;
+// var bottom = 0;
+// function newsScroll() {
+//     if(bottom > (window.screen.availHeight - window.screenTop)) {
+//         bottom = 0;
+//         oArrow.style.bottom = 0;
+//     }else {
+//         bottom = bottom + 15;
+//         oArrow.style.bottom = bottom;
+//     }
+// }
+// var timeid = setInterval(newsScroll,300);
+// function ClearTimeInterVal() {
+//     clearInterval(timeid);
+// }
+// function resetInterVal() {
+//     timeid = setInterval(newsScroll,300);
+// }
