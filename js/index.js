d281ada47f834db3d4a78dfc4ad1f8db33bf6edf
AsaZws <zhouws.com@gmail.com>
2018-10-26 17:59:43 +0800

    逻辑

--- js/index.js
+++ js/index.js
@@ -1,23 +1,4 @@
 
-// 箭头的class
-// var oArrow = document.getElementsByClassName('.arrow');
-
-// oArrow.style.bottom = 0;
-// oArrow.style = window.screen.availWidth-100;
-// var bottom = 0;
-// function newsScroll() {
-//     if(bottom > (window.screen.availHeight - window.screenTop)) {
-//         bottom = 0;
-//         oArrow.style.bottom = 0;
-//     }else {
-//         bottom = bottom + 15;
-//         oArrow.style.bottom = bottom;
-//     }
-// }
-// var timeid = setInterval(newsScroll,300);
-// function ClearTimeInterVal() {
-//     clearInterval(timeid);
-// }
-// function resetInterVal() {
-//     timeid = setInterval(newsScroll,300);
-// }
+// 点击进入切换到下一页
+var oSpom8 = document.getElementsByClassName(".spom8");
+oSpom8.style.display = "none";
