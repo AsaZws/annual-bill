d4b4d48ccb21cc1175696968fd52066866868789
AsaZws <zhouws.com@gmail.com>
2018-11-23 19:51:38 +0800

    第三页

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
-// 点击进入切换到下一页
-// var oSpom8 = document.getElementsByClassName(".spom8");
-// oSpom8.style.display = "none";
+
 
 active();
 function active() {
@@ -13,6 +11,8 @@ function active() {
           first();
         } else if (myswiper.activeIndex === 1) {
             second();
+        } else if (myswiper.activeIndex === 2) {
+          third();
         }
       }
     },
@@ -39,23 +39,24 @@ function active() {
   var html = document.documentElement;
   var width = html.getBoundingClientRect().width;
   t1 = new TimelineMax();
+  // 开关
   var onOff = true;
   first();
   function first() {
     if (onOff) {
       // 圆掉下
-      t1.from(".spom1", 1.5, {
+      t1.from(".first-spom1", 1.5, {
         y: -300,
         ease: Bounce.easeOut,
         autoAlpha: 0
       })
         // 手机
-        .from(".spom2", 1, {
+        .from(".first-spom2", 1, {
           autoAlpha: 0,
           height: 0
         })
         .from(
-          ".spom3",
+          ".first-spom3",
           0.8,
           {
             autoAlpha: 0
@@ -63,7 +64,7 @@ function active() {
           0.7
         )
         .to(
-          ".spom3",
+          ".first-spom3",
           0.8,
           {
             y: 8,
@@ -74,19 +75,19 @@ function active() {
         )
         // 账单
         .from(
-          ".spom4",
+          ".first-spom4",
           1,
           {
             autoAlpha: 0
           },
           0.8
         )
-        .set(".spom4", {
+        .set(".first-spom4", {
           transformPerspective: 10,
           transformOrigin: "left top"
         })
         .to(
-          ".spom4",
+          ".first-spom4",
           1.5,
           {
             rotationY: 1800
@@ -113,51 +114,72 @@ function active() {
   }
 
   //   ------------------第二屏--------------------------
-  var data = document.querySelector(".data");
-  var datap = data.querySelectorAll("span");
-  var t3 = new TimelineMax();
-  var onOff3 = true;
+  var t2 = new TimelineMax();
+  var onOff2 = true;
   second();
   function second() {
-      if(onOff3) {
+      if(onOff2) {
         //   文字
-        t3.staggerFrom(".data p", 1, {
+        t2.staggerFrom(".data1 p", 1, {
             x: -300,
         }, 0.3)
+        // 城市
+        .from(".second-img1", 1.5, {
+          alpha: 0,
+        }, 1)
         // 小车
-        .from(".second-img6", 1, {
+        .from(".second-img8", 1, {
             x: 300,
             y: -240,
-        }, 0.5)
+        }, 1)
+        .from(".second-img3", 1.5, {
+          alpha: 0,
+          x: -50,
+          y: 25,
+        }, 1)
         // 定位
-        .from(".second-img11", 1.1, {
+        .from(".second-img2", 1.1, {
             y: -200,
             alpha: 0,
             ease: Bounce.easeOut,
         }, 1)
-        onOff3 = false;
+        onOff2 = false;
       } else {
-        t3.restart();
+        t2.restart();
       }
   }
-
-  // ------------------第三屏底部数据加载-----------------------
-  var oUl1 = document.getElementById("bottomUl");
-  var figure_bottom = ["微信缴费", "无感支付", "电子钱包", "支付宝付"];
-  figure_bottom.sort(function(i, l) {
-    return i - l;
-  });
-  var oLi1 = oUl1.getElementsByTagName("li");
-  for (var k = 0; k < figure_bottom.length; k++) {
-    oLi1[k].firstChild.innerHTML = figure_bottom[k];
-  }
-  // 柱状图列表高度加载和顶部数据加载
-  var figure_height = ["333", "0", "1860", "120"];
-  var mid1 = document.getElementById("midUl");
-  var midLi = mid1.getElementsByTagName("li");
-  for (var j = 0; j < figure_height.length; j++) {
-    midLi[j].style.height = figure_height[j] * 0.003 + "rem";
-    midLi[j].firstChild.innerHTML = figure_height[j];
-    midLi[j].style.bottom = -(7 - figure_height[j] * 0.003) + "rem";
+  // --------------------------第三屏--------------------------
+  var t3 = new TimelineMax();
+  var onOff3 = true;
+  third();
+  function third() {
+    if(onOff3) {
+      // 文字
+      t3.staggerFrom(".data2 p", 1, {
+        x: 300,
+      }, 0.3)
+      offOn3 = false;
+    }else {
+      t3.restart();
+    }
+    // ------------------第三屏底部数据加载-----------------------
+    var oUl1 = document.getElementById("bottomUl");
+    var figure_bottom = ["微信缴费", "无感支付", "电子钱包", "支付宝付"];
+    figure_bottom.sort(function(i, l) {
+      return i - l;
+    });
+    var oLi1 = oUl1.getElementsByTagName("li");
+    for (var k = 0; k < figure_bottom.length; k++) {
+      oLi1[k].firstChild.innerHTML = figure_bottom[k];
+    }
+    // 柱状图列表高度加载和顶部数据加载
+    var figure_height = ["333", "0", "1860", "120"];
+    var mid1 = document.getElementById("midUl");
+    var midLi = mid1.getElementsByTagName("li");
+    for (var j = 0; j < figure_height.length; j++) {
+      midLi[j].style.height = figure_height[j] * 0.003 + "rem";
+      midLi[j].firstChild.innerHTML = figure_height[j];
+      midLi[j].style.bottom = -(7 - figure_height[j] * 0.003) + "rem";
+    }
   }
 }
