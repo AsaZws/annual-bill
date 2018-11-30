f6b78499debf25dec3a96e17a79f367000242a03
AsaZws <zhouws.com@gmail.com>
2018-11-30 18:15:52 +0800

    完成动画

--- js/index.js
+++ js/index.js
@@ -100,14 +100,6 @@ function active() {
         .from(".first p", 1, {
           autoAlpha: 0
         })
-        .from(
-          ".btn",
-          0.3,
-          {
-            x: 400
-          },
-          2.2
-        );
       // 关掉开关
       onOff = false;
     } else {
@@ -249,134 +241,227 @@ function active() {
     }
   }
   // -----------------------第五屏-----------------------
-var t4 = new TimelineMax();
-var offOn4 = true;
-fourth();
-function fourth() {
-  if (offOn4) {
-    // 恭喜你获得
-    t4.from(".fourth-img1",1, {
-      alpha: 0,
-    }, 1.6)
-    .set(".fourth-img1", {
-      transformPerspective: 10,
-      transformOrigin: "right top",
-    })
-    .to(
-      ".fourth-img1",
-      1,
-      {
-        rotationX: 360,
-        transformOrigin: "right top",
-      }, 1.4)
-    .from(".fourth-img17", 0.2, {
-      alpha: 0,
-      y: 100,
-    }, 0.3)
-    .fromTo(".fourth-img3", 1, {
-      alpha: 0,
-      x: -100,
-      y: 56,
-    },  {
-      alpha: 1,
-      x: 0,
-      y: 0,
-    }, 0.5)
-    .fromTo(".fourth-img4", 1, {
-      alpha: 0,
-      x: 100,
-      y: 56,
-    }, {
-      alpha: 1,
-      x: 0,
-      y: 0,
-    }, 0.5)
-    // 中间金币
-    .from(
-      ".fourth-img5",
-      1.1,
-      {
-        y: -200,
-        alpha: 0,
-        ease: Bounce.easeOut
-      },
-      1
-    )
-    // 左边金币
-    .fromTo(".fourth-img6", 1, {
-      alpha: 0,
-      x: -20,
-      y: 10,
-    }, {
-      alpha: 1,
-      x: 0,
-      y: 0,
-    }, 0.5)
-    // 右边云
-    .from(".fourth-img7", 1, {
-      alpha: 0,
-      x: 100,
-      y: 100,
-    }, 0.5)
-    // 左侧云
-    .from(".fourth-img8", 1, {
-      alpha: 0,
-      x: -100,
-      y: 100,
-    }, 0.5)
-    // 黄色云
-    .from(".fourth-img12", 1, {
-      alpha: 0,
-      x: 100,
-      y: 100,
-    }, 0.5)
-    // 树
-    .from(".fourth-img9", 1, {
-      alpha: 0,
-      width: 0,
-      height: 0,
-      y: -100,
-    }, 0.8)
-    // 左边竖线
-    .from(".fourth-img10", 1.4, {
-      alpha: 0,
-    }, 0.5)
-    // 左边竖线
-    .from(".fourth-img11", 1.4, {
-      alpha: 0,
-    }, 0.5)
-    // 奖杯
-    .from(".fourth-img18", 1, {
-      y: 200,
-    }, 1)
-    // logo
-    .from(".fourth-img13", 1, {
-      alpha: 0,
-      y: -200,
-    }, 2)
-    // 光
-    .from(".fourth-img14", 1, {
-      alpha: 0,
-      y: 100,
-    }, 2.2)
-    // 称号
-    .staggerFrom(".fourth-img2", 1, {
-      alpha: 0,
-      rotation: 1440,
-      y: -100,
-    }, 0.8)
-    // 称号上下浮动
-    .to(".fourth-img2", 1, {
-      y: -10,
-      repeat: -1,
-      yoyo: true,
-    })
-    offOn4 = false;
-  } else {
-    t4.restart();
+  var t4 = new TimelineMax();
+  var offOn4 = true;
+  fourth();
+  function fourth() {
+    if (offOn4) {
+      // 恭喜你获得
+      t4.from(
+        ".fourth-img1",
+        1,
+        {
+          alpha: 0
+        },
+        1.6
+      )
+        .set(".fourth-img1", {
+          transformPerspective: 10,
+          transformOrigin: "right top"
+        })
+        .to(
+          ".fourth-img1",
+          1,
+          {
+            rotationX: 360,
+            transformOrigin: "right top"
+          },
+          1.4
+        )
+        .from(
+          ".fourth-img17",
+          0.2,
+          {
+            alpha: 0,
+            y: 100
+          },
+          0.3
+        )
+        .fromTo(
+          ".fourth-img3",
+          1,
+          {
+            alpha: 0,
+            x: -100,
+            y: 56
+          },
+          {
+            alpha: 1,
+            x: 0,
+            y: 0
+          },
+          0.5
+        )
+        .fromTo(
+          ".fourth-img4",
+          1,
+          {
+            alpha: 0,
+            x: 100,
+            y: 56
+          },
+          {
+            alpha: 1,
+            x: 0,
+            y: 0
+          },
+          0.5
+        )
+        // 中间金币
+        .from(
+          ".fourth-img5",
+          1.1,
+          {
+            y: -200,
+            alpha: 0,
+            ease: Bounce.easeOut
+          },
+          1
+        )
+        // 左边金币
+        .fromTo(
+          ".fourth-img6",
+          1,
+          {
+            alpha: 0,
+            x: -20,
+            y: 10
+          },
+          {
+            alpha: 1,
+            x: 0,
+            y: 0
+          },
+          0.5
+        )
+        // 右边云
+        .from(
+          ".fourth-img7",
+          1,
+          {
+            alpha: 0,
+            x: 100,
+            y: 100
+          },
+          0.5
+        )
+        // 左侧云
+        .from(
+          ".fourth-img8",
+          1,
+          {
+            alpha: 0,
+            x: -100,
+            y: 100
+          },
+          0.5
+        )
+        // 黄色云
+        .from(
+          ".fourth-img12",
+          1,
+          {
+            alpha: 0,
+            x: 100,
+            y: 100
+          },
+          0.5
+        )
+        // 树
+        .from(
+          ".fourth-img9",
+          1,
+          {
+            alpha: 0,
+            width: 0,
+            height: 0,
+            y: -100
+          },
+          0.8
+        )
+        // 左边竖线
+        .from(
+          ".fourth-img10",
+          1.4,
+          {
+            alpha: 0
+          },
+          0.5
+        )
+        // 左边竖线
+        .from(
+          ".fourth-img11",
+          1.4,
+          {
+            alpha: 0
+          },
+          0.5
+        )
+        // 奖杯
+        .from(
+          ".fourth-img18",
+          1,
+          {
+            y: 200
+          },
+          1
+        )
+        // logo
+        .from(
+          ".fourth-img13",
+          1,
+          {
+            alpha: 0,
+            y: -200
+          },
+          2
+        )
+        // 光
+        .from(
+          ".fourth-img14",
+          1,
+          {
+            alpha: 0,
+            y: 100
+          },
+          2.2
+        )
+        // 称号
+        .staggerFrom(
+          ".fourth-img2",
+          1,
+          {
+            alpha: 0,
+            rotation: 1440,
+            y: -100
+          },
+          0.8
+        )
+        // 称号上下浮动
+        .to(".fourth-img2", 1, {
+          y: -10,
+          repeat: -1,
+          yoyo: true
+        });
+      offOn4 = false;
+    } else {
+      t4.restart();
+    }
   }
-}
 
+  // 定义数组方法，删除数组固定数
+  Array.prototype.indexOf = function(val) {
+    for (var i = 0; i < this.length; i++) {
+      if (this[i] == val) return i;
+    }
+    return -1;
+  };
+  Array.prototype.remove = function(val) {
+    var index = this.indexOf(val);
+    if (index > -1) {
+      this.splice(index, 1);
+    }
+  };
   // -----------------------第五屏-----------------------
   var t5 = new TimelineMax();
   var offOn5 = true;
@@ -385,50 +470,76 @@ function fourth() {
     if (offOn5) {
       t5.from(".score p", 1, {
         alpha: 0,
-        x: -200,
+        x: -200
       })
-      .from(".star", 1, {
-        alpha: 0,
-        height: 0,
-      })
-      .from(".suggest p", 1, {
-        alpha: 0,
-        x: -200,
-      })
-      .staggerFrom(".suggest strong", 1, {
-        alpha: 0,
-        rotation: 360,
-        y: 100,
-      }, 0.5)
+        .from(".star", 1, {
+          alpha: 0,
+          height: 0
+        })
+        .from(".suggest p", 1, {
+          alpha: 0,
+          x: -200
+        })
+        .staggerFrom(
+          ".suggest strong",
+          1,
+          {
+            alpha: 0,
+            rotation: 360,
+            y: 100
+          },
+          0.1
+        )
+        .from(
+          ".btn",
+          0.3,
+          {
+            x: 400
+          }
+        );
       offOn5 = false;
     } else {
       t5.restart();
     }
     var oStar = document.getElementsByClassName("star")[0];
+    // 获取li
     var oLi0 = oStar.getElementsByTagName("ul")[0].getElementsByTagName("li");
     var oLi1 = oStar.getElementsByTagName("ul")[1].getElementsByTagName("li");
     var oLi2 = oStar.getElementsByTagName("ul")[2].getElementsByTagName("li");
+    // 获取span
+    var oSpan0 = oStar.getElementsByTagName("ul")[0].getElementsByTagName("span")[0];
+    var oSpan1 = oStar.getElementsByTagName("ul")[1].getElementsByTagName("span")[0];
+    var oSpan2 = oStar.getElementsByTagName("ul")[2].getElementsByTagName("span")[0];
     // 修改建议
     var oStrong = document
       .getElementsByClassName("suggest")[0]
       .getElementsByTagName("strong");
+    var arr = [];
     for (var i = 0; i < oStrong.length; i++) {
       oStrong[i].index = i;
       oStrong[i].onOff = true;
+      // 给没条建议添加点击事件
       oStrong[i].onclick = function() {
+        var arr_strong = this.innerHTML;
         if (this.onOff) {
           this.style.color = "#fff";
           this.style.backgroundColor = "#f6b617";
           this.onOff = false;
+          // 选择添入数组
+          arr.push(arr_strong);
         } else {
           this.style.color = "#e4f0fe";
           this.style.backgroundColor = "#212a60";
           this.onOff = true;
+          var arr_strong_f = this.innerHTML;
+          // 点击从数组中删除
+          arr.remove(arr_strong_f);
         }
-        console.log(this.onOff);
+        console.log(arr);
       };
     }
-    function star(li) {
+    // 星星评分
+    function star(li, span) {
       // 评分返回的结果
       // var oscore = [];
       var num = 0;
@@ -440,18 +551,20 @@ function fourth() {
             li[i].style.backgroundImage = "url('./images/fo-star2.png')";
           }
           // 当点击当前星星，之前的都点亮包含自己
-          num = this.index;
+          num = this.index + 1;
           for (var i = 0; i <= this.index; i++) {
             li[i].style.backgroundImage = "url('./images/fo-star1.png')";
           }
+          var oSpan_s = span.innerHTML;
           // 先清空
-          // oscore = [""];
+          // oscore.push(oSpan_s);
+          console.log(oSpan_s);
           console.log(num);
         };
       }
     }
-    star(oLi0);
-    star(oLi1);
-    star(oLi2);
+    star(oLi0, oSpan0);
+    star(oLi1, oSpan1);
+    star(oLi2, oSpan2);
   }
 }
